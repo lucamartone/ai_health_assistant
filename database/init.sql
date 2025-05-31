0cbf8c28077bd091ef00c03f75299a94da8207cd
markoldmark <marcoldmark@gmail.com>
2025-05-31 17:33:04 +0200

    bozza database

---
+++ database/init.sql
@@ -0,0 +1,51 @@
+
+
+
+CREATE TABLE utente (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    reset_token VARCHAR(255),
+    token_expiration TIMESTAMP,
+    saldo DECIMAL(10, 2) DEFAULT 0.00,
+    sesso CHAR(1) CHECK (sesso IN ('M', 'F')),
+    foto_profilo BLOB,
+    UNIQUE (username, email)
+);
+
+CREATE TABLE paziente (
+    id SERIAL PRIMARY KEY,
+    id_paziente INT NOT NULL,
+    data_nascita DATE NOT NULL,
+    FOREIGN KEY (id_paziente) REFERENCES utente(id)
+);
+
+CREATE TABLE medico (
+    id SERIAL PRIMARY KEY,
+    specializzazione VARCHAR(50) NOT NULL,
+    rank INT NOT NULL,
+    id_medico INT NOT NULL,
+    FOREIGN KEY (id_medico) REFERENCES utente(id)
+);
+
+CREATE TABLE cronologia (
+    id SERIAL PRIMARY KEY,
+    id_paziente INT NOT NULL,
+    id_appuntamento INT NOT NULL,
+    resoconto TEXT,
+    valutazione INT CHECK (valutazione BETWEEN 1 AND 5),
+    FOREIGN KEY (id_paziente) REFERENCES paziente(id),
+    FOREIGN KEY (id_appuntamento) REFERENCES appuntamento(id)
+);
+
+CREATE TABLE appuntamento (
+    id SERIAL PRIMARY KEY,
+    id_medico INT NOT NULL,
+    id_utente INT NOT NULL,
+    data_ora TIMESTAMP NOT NULL,
+    stato VARCHAR(20) DEFAULT 'in attesa',
+    FOREIGN KEY (id_medico) REFERENCES medico(id),
+    FOREIGN KEY (id_utente) REFERENCES utente(id)
+);
