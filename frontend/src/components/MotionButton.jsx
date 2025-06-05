2a3e5b456222c99f0da59daaecbe9e95517a14aa
markoldmark <marcoldmark@gmail.com>
2025-06-05 23:44:51 +0200

    favicon

--- frontend/src/components/MotionButton.jsx
+++ frontend/src/components/MotionButton.jsx
@@ -3,16 +3,13 @@ import { motion } from 'framer-motion';
 function MotionButton({ children, onClick }) {
   return (
     <motion.button
-      whileHover={{
-        scale: 1.05,
-        transition: { duration: 0.1, ease: 'easeOut' }
-      }}
+      whileHover={{ scale: 1.05 }} // rimosso transition per effetto istantaneo
       whileTap={{
         scale: 0.95,
         transition: { duration: 0.1, ease: 'easeIn' }
       }}
       onClick={onClick}
-      className="px-6 py-2 md:px-8 md:py-3 bg-white text-slate-700 text-sm md:text-base font-medium border border-slate-200 rounded-lg hover:bg-slate-50 transition-all duration-300 shadow-sm hover:shadow-md"
+      className="px-6 py-2 md:px-8 md:py-3 bg-white text-blue-700 text-sm md:text-base font-medium border border-slate-200 rounded-lg hover:bg-slate-50 transition-all duration-300 shadow-sm hover:shadow-md"
     >
       {children}
     </motion.button>
