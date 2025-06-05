f0892610a351525f470c1f16a69e5c4ca643d5bb
markoldmark <marcoldmark@gmail.com>
2025-06-05 23:48:32 +0200

    gradient

--- frontend/src/components/MotionButton.jsx
+++ frontend/src/components/MotionButton.jsx
@@ -3,13 +3,13 @@ import { motion } from 'framer-motion';
 function MotionButton({ children, onClick, gradient}) {
   return (
     <motion.button
-      whileHover={{ scale: 1.05 }} // rimosso transition per effetto istantaneo
+      whileHover={{ scale: 1.05 }}
       whileTap={{
         scale: 0.95,
         transition: { duration: 0.1, ease: 'easeIn' }
       }}
       onClick={onClick}
-      className="px-6 py-2 md:px-8 md:py-3 bg-white text-slate-700 text-sm md:text-base font-medium border border-slate-200 rounded-lg hover:bg-slate-50 transition-all duration-300 shadow-sm hover:shadow-md"
+      className={`px-6 py-2 md:px-8 md:py-3 bg-gradient-to-r ${gradient} text-slate-700 text-sm md:text-base font-medium border border-slate-200 rounded-lg hover:bg-slate-50 transition-all duration-300 shadow-sm hover:shadow-md`}
     >
       {children}
     </motion.button>
