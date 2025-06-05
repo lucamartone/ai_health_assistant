45a7e09783c8ea14b93314adc64abf7b0f79ae82
lucamartone <luca.martone14@gmail.com>
2025-06-05 23:24:56 +0200

    aggiunto gradiente     aggiunto gradiente

--- frontend/src/components/MotionButton.jsx
+++ frontend/src/components/MotionButton.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 
-function MotionButton({ children, onClick, gradient, hover }) {
+function MotionButton({ children, onClick, gradient}) {
   return (
     <motion.button
       whileHover={{
@@ -12,7 +12,7 @@ function MotionButton({ children, onClick, gradient, hover }) {
         transition: { duration: 0.1, ease: 'easeIn' }
       }}
       onClick={onClick}
-      className={`w-full px-6 py-3 bg-gradient-to-r ${gradient} text-white rounded-xl hover:shadow-lg transition-all duration-300 shadow-sm flex items-center justify-center gap-2 group ${hover}`}
+      className={`w-full px-6 py-3 bg-gradient-to-r ${gradient} text-white rounded-xl hover:shadow-lg transition-all duration-300 shadow-sm flex items-center justify-center gap-2 group`}
     >
       {children}
     </motion.button>
