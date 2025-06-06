0148f701de24fad0c97adf7c41a8a2693d21c43a
markoldmark <marcoldmark@gmail.com>
2025-06-06 17:00:07 +0200

    database corretto

--- database/init.sql
+++ database/init.sql
@@ -11,7 +11,7 @@ FLUSH PRIVILEGES;
 
 
 CREATE TABLE utente (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     cognome VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
@@ -20,27 +20,36 @@ CREATE TABLE utente (
     token_expiration TIMESTAMP,
     saldo DECIMAL(10, 2) DEFAULT 0.00,
     sesso CHAR(1) CHECK (sesso IN ('M', 'F')),
-    foto_profilo BLOB,
-    UNIQUE (username, email)
+    foto_profilo BLOB
 );
 
 CREATE TABLE paziente (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_paziente INT NOT NULL,
     data_nascita DATE NOT NULL,
     FOREIGN KEY (id_paziente) REFERENCES utente(id)
 );
 
 CREATE TABLE medico (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     specializzazione VARCHAR(50) NOT NULL,
     rank INT NOT NULL,
     id_medico INT NOT NULL,
     FOREIGN KEY (id_medico) REFERENCES utente(id)
 );
 
+CREATE TABLE appuntamento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_medico INT NOT NULL,
+    id_utente INT NOT NULL,
+    data_ora TIMESTAMP NOT NULL,
+    stato VARCHAR(20) DEFAULT 'in attesa',
+    FOREIGN KEY (id_medico) REFERENCES medico(id),
+    FOREIGN KEY (id_utente) REFERENCES utente(id)
+);
+
 CREATE TABLE cronologia (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_paziente INT NOT NULL,
     id_appuntamento INT NOT NULL,
     resoconto TEXT,
@@ -49,12 +58,3 @@ CREATE TABLE cronologia (
     FOREIGN KEY (id_appuntamento) REFERENCES appuntamento(id)
 );
 
-CREATE TABLE appuntamento (
-    id SERIAL PRIMARY KEY,
-    id_medico INT NOT NULL,
-    id_utente INT NOT NULL,
-    data_ora TIMESTAMP NOT NULL,
-    stato VARCHAR(20) DEFAULT 'in attesa',
-    FOREIGN KEY (id_medico) REFERENCES medico(id),
-    FOREIGN KEY (id_utente) REFERENCES utente(id)
-);
